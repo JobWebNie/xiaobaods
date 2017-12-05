848a543fa8c4f7af8f94490926ba374ae201bcb6
niesaisai <1979510089@qq.com>
2017-12-05 18:53:47 +0800

    在element组件的基础上二次封装common-chin组件实现筛选功能

---
+++ src/components/common/Chin.vue
@@ -0,0 +1,71 @@
+<style scoped>
+  .comment-Chin {
+    padding: 0 20px 0 30px;
+  }
+  .comment-Chin .el-main span{
+    display:inline-block;
+    width:100px;
+  }
+</style>
+<template>
+  <div class="comment-Chin">
+    <el-container>
+      <el-aside width="200px">
+        <h3>{{chinTitle}}</h3>
+      </el-aside>
+      <el-main>
+        <span v-for="option in options">
+          <el-select v-model="value">
+            <el-option v-for="item in option" :key="item.value" :label="item.label" :value="item.value">
+            </el-option>
+          </el-select>
+        </span>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      chinTitle: {
+        type: String
+      }
+    },
+    data(){
+      return {
+        options:[[{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }],
+        [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }],
+        [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }]],
+        value: ''
+      }
+    }
+  }
+
+</script>
