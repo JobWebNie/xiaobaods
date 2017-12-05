1bf8ff754f6c9cad03158bfbd1dbe71e9a08b997
niesaisai <1979510089@qq.com>
2017-12-05 18:53:03 +0800

    "将需要改版的项目配置好前端路由"

--- src/router/index.js
+++ src/router/index.js
@@ -16,21 +16,63 @@ export default new Router({
       path: '/login/',
       name: 'UserLogin',
       component: function (resolve) {
-        require(['../components/user/Login.vue'], resolve)
+        require(['@/components/user/Login.vue'], resolve)
       }
     },
     {
       path: '/select/',
       name: 'UserTest',
       component: function (resolve) {
-        require(['../components/common/Select.vue'], resolve)
+        require(['@/components/common/Select.vue'], resolve)
+      }
+    },
+    {
+      path: '/product',
+      name: 'ProductTrend',
+      component: function (resolve) {
+        require(['@/components/page/Product.vue'], resolve)
+      }
+    },
+    {
+      path: '/world',
+      name: 'ProductWorld',
+      component: function (resolve) {
+        require(['@/components/page/World.vue'], resolve)
+      }
+    },
+    {
+      path: '/property',
+      name: 'ProductProperty',
+      component: function (resolve) {
+        require(['@/components/page/Property.vue'], resolve)
+      }
+    },
+    {
+      path: '/property-deal',
+      name: 'ProductPropertyDeal',
+      component: function (resolve) {
+        require(['@/components/page/PropertyDeal.vue'], resolve)
+      }
+    },
+    {
+      path: '/weekport',
+      name: 'WeekportTrend',
+      component: function (resolve) {
+        require(['@/components/page/Weekport.vue'], resolve)
+      }
+    },
+    {
+      path: '/tool-box',
+      name: 'ToolBox',
+      component: function (resolve) {
+        require(['@/components/page/ToolBox.vue'], resolve)
       }
     },
     {
       path: '*',
       name: '404Page',
       component: function (resolve) {
-        require(['../components/404page.vue'], resolve)
+        require(['@/components/404page.vue'], resolve)
       }
     }
   ]
